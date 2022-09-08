f9ed4d345e2ea242a7f0e8b6e8c6cf82beff13f4
Sean Conrad <sean.w.conrad@gmail.com>
2022-09-08 08:33:17 -0400

    added to server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const app = express();
 const PORT = process.env.port || 3001;
 const apiRoutes = require('./routes/apiroutes');
 const htmlRoutes = require('./routes/htmlroutes');
+const fs = require ('fs');
+const util = require('util');
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+app.use(express.static("./public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
