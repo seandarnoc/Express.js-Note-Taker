8d9b71f9fe962288e015c687818441f189d8c650
Sean Conrad <sean.w.conrad@gmail.com>
2022-09-10 08:28:52 -0400

    changed folders

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 const app = express();
 
 const apiRoutes = require("./routes/api/apiRoutes.js");
-const htmlRoutes = require("/routes/html/htmlRoutes.js");
+const htmlRoutes = require("./routes/html/htmlRoutes.js");
 
 const PORT = process.env.port || 3001;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use( express.static("public"));
+app.use(express.static("public"));
 
 
 app.use('/api', apiRoutes);
@@ -19,5 +19,5 @@ app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => 
-  console.log(`Serving  at http://localhost:${PORT}!`)
+  console.log(`Serving  at http://localhost:${PORT}`)
 );
